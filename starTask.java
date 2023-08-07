898b80935f34c50b21512ccb974b09a87dd1d71e
AzzaAlkiyumi <138677162+AzzaAlkiyumi@users.noreply.github.com>
2023-08-08 01:51:27 +0400

    Add files via upload

---
+++ starTask.java
@@ -0,0 +1,12 @@
+public class starTask {
+    public static void main(String[] args) {
+
+        //Stars task
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
